e97769b51436ed0083eb136c4d2dc3f73b8e7369
sivam777 <sivam.evt@gmail.com>
2016-03-03 20:31:19 +0200

    Update DISCR.c

--- DISCR.c
+++ DISCR.c
@@ -8,15 +8,14 @@ int main (void)
 	int a, b, c;
 	float D, x1, x2;
 	
-	setlocale(LC_ALL,"Russian");
 	printf("\n\nbudem reshat uravnenie  a*x^2+b*x+c=0\n");
 	printf("\nvvedite a: ");
 	scanf("%d", &a);
 	printf("\nvvedite b: ");
 	scanf("%d", &b);
-	printf("ââåäèòå c: ");
+	printf("\nvvedite c: ");
 	scanf("%d", &c);
-	printf("\nðåøàåì óðàâíåíèå %d*x^2+%d*x+%d=0\n\n", a, b, c);
+	printf("\nreshaem uravnenie %d*x^2+%d*x+%d=0\n\n", a, b, c);
 	
 	D = b * b - (4 * a * c);
 	if (D >= 0) {
@@ -24,11 +23,11 @@ int main (void)
 			x2 = (- b - sqrt(D)) / (2 * a);
 			printf("\nx1 = %.1f\tx2 = %.1f\n\n\n", x1, x2);
 		} else {
-			printf("\níåò äåéñòâèòåëüíûõ îòâåòîâ\n\n\n");
+			printf("\nnet otvetov\n\n\n");
 		}
 		
-	printf("ââåäèòå 'y', åñëè õîòèòå ïðîäîëæèòü,\n"
-		"èëè ëþáîé äðóãîé ñèìâîë, åñëè õîòèòå çàêîí÷èòü: ");
+	printf("vvedite 'y', esle hotite prodolgit,\n"
+		"ili luboy drugoy simvol esle hotite zakonshit: ");
 		
 	scanf("%s", &y);
 	
